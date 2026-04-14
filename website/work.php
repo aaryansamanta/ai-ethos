5de804d9fbdae7c25d5aa00e6d377ee951fd01d5
aaryansamanta <aaryansamanta@domain.com>
2026-04-14 23:57:23 +0800

    add timeline

--- website/work.php
+++ website/work.php
@@ -50,23 +50,211 @@
 
         <!-- about area -->
         <div class="about-area py-120">
-            <div id="events" class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-6">
+            <div id="events" class="container pb-60">
+                <div class="row">
+                    <div class="col-lg-5">
                         <div class="about-left">
                             <div class="about-img">
                                 <img src="assets/img/about/01.png" alt="">
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-6">
+                    <div class="col-lg-7">
                         <div class="about-right">
-                            <div class="site-heading mb-3">
+                            <div class="site-heading mb-3 text-center">
                                 <span class="site-title-tagline">Program</span>
                                 <h2 class="site-title">Our <span>Events</span></h2>
                             </div>
-                            <h3 class="mt-3">TBD</h3>
-                            <p class="about-text">info@aiethos.org (Programs)</p>
+                            <style>
+                                .timeline {
+                                    position: relative;
+                                }
+
+                                .timeline::before {
+                                    content: '';
+                                    display: block;
+                                    position: absolute;
+                                    top: 0;
+                                    left: calc(50% - 1px);
+                                    width: 2px;
+                                    height: 100%;
+                                    background-color: #520068;
+                                    z-index: -1;
+                                }
+
+                                .timeline li {
+                                    display: flex;
+                                }
+
+                                .timeline li+li {
+                                    margin-top: 30px;
+                                }
+
+                                .timeline li:nth-child(even) {
+                                    flex-direction: row-reverse;
+                                }
+
+                                .timeline-date,
+                                .timeline-content {
+                                    width: 50%;
+                                    padding: 0 20px;
+                                }
+
+                                .timeline-date {
+                                    line-height: 20px;
+                                }
+
+                                .timeline-date span {
+                                    display: inline-block;
+                                    margin: 15px 0;
+                                    padding: 5px 15px;
+                                    border-radius: 20px;
+                                    background: linear-gradient(to right, #520068, #bb64d2);
+                                    color: #fff;
+                                }
+
+                                .timeline li:nth-child(odd) span {
+                                    float: right;
+                                }
+
+                                .timeline li:nth-child(even) span {
+                                    float: left;
+                                }
+
+                                .timeline-point {
+                                    width: 20px;
+                                    height: 20px;
+                                    margin-top: 20px;
+                                    border: 2px solid #fff;
+                                    border-radius: 10px;
+                                    background-color: #520068;
+                                }
+
+                                .timeline li:nth-child(even) .timeline-content {
+                                    text-align: right;
+                                }
+
+                                .timeline-content p {
+                                    padding: 20px;
+                                    border-radius: 10px;
+                                    background-color: #f7ebfb;
+                                }
+
+                                .timeline-title {
+                                    display: flex;
+                                    align-items: center;
+                                    height: 60px;
+                                    margin-bottom: 10px;
+                                    overflow: hidden;
+                                }
+
+                                .timeline-title h6 {
+                                    flex: 1;
+                                    line-height: 30px;
+                                }
+                            </style>
+                            <ul class="timeline">
+                                <li>
+                                    <div class="timeline-date"><span>3/12/25</span></div>
+                                    <div class="timeline-point"></div>
+                                    <div class="timeline-content">
+                                        <div class="timeline-title">
+                                            <h6>Research on AI tutoring equity gaps begins</h6>
+                                        </div>
+                                        <p>Research on AI tutoring equity gaps begins</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="timeline-date"><span>3/28/25</span></div>
+                                    <div class="timeline-point"></div>
+                                    <div class="timeline-content">
+                                        <div class="timeline-title">
+                                            <h6>Mission framework drafted</h6>
+                                        </div>
+                                        <p>Mission framework drafted</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="timeline-date"><span>4/11/25</span></div>
+                                    <div class="timeline-point"></div>
+                                    <div class="timeline-content">
+                                        <div class="timeline-title">
+                                            <h6>AI Ethos ideation</h6>
+                                        </div>
+                                        <p>AI Ethos ideation</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="timeline-date"><span>7/30/25</span></div>
+                                    <div class="timeline-point"></div>
+                                    <div class="timeline-content">
+                                        <div class="timeline-title">
+                                            <h6>Incorporation approved</h6>
+                                        </div>
+                                        <p>Incorporation approved</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="timeline-date"><span>9/26/25</span></div>
+                                    <div class="timeline-point"></div>
+                                    <div class="timeline-content">
+                                        <div class="timeline-title">
+                                            <h6>IRS 501(c)(3) approval</h6>
+                                        </div>
+                                        <p>IRS 501(c)(3) approval</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="timeline-date"><span>10/5/25</span></div>
+                                    <div class="timeline-point"></div>
+                                    <div class="timeline-content">
+                                        <div class="timeline-title">
+                                            <h6>Website v1 launched</h6>
+                                        </div>
+                                        <p>Website v1 launched</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="timeline-date"><span>10/15/25</span></div>
+                                    <div class="timeline-point"></div>
+                                    <div class="timeline-content">
+                                        <div class="timeline-title">
+                                            <h6>Tutoring launched for 25 students in 1 country</h6>
+                                        </div>
+                                        <p>Tutoring launched for 25 students in 1 country</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="timeline-date"><span>12/1/25</span></div>
+                                    <div class="timeline-point"></div>
+                                    <div class="timeline-content">
+                                        <div class="timeline-title">
+                                            <h6>Partnership with LCP</h6>
+                                        </div>
+                                        <p>Partnership with LCP</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="timeline-date"><span>2/12/26</span></div>
+                                    <div class="timeline-point"></div>
+                                    <div class="timeline-content">
+                                        <div class="timeline-title">
+                                            <h6>Neurodiverse-learning module released</h6>
+                                        </div>
+                                        <p>Neurodiverse-learning module released</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="timeline-date"><span>4/12/26</span></div>
+                                    <div class="timeline-point"></div>
+                                    <div class="timeline-content">
+                                        <div class="timeline-title">
+                                            <h6>Impact milestone: 195 learners across 3 countries</h6>
+                                        </div>
+                                        <p>Impact milestone: 195 learners across 3 countries</p>
+                                    </div>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </div>
